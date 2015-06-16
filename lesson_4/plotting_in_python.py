a5f22ac4e8d2142e9a39a757a1e1863128969e99
Terry Bates <terryjbates@gmail.com>
2015-06-16 01:26:14 -0700

    Added Graphical Perception paper, hr_year data, and plotting_in_python.py script.

---
+++ lesson_4/plotting_in_python.py
@@ -0,0 +1,29 @@
+from pandas import *
+from ggplot import *
+
+import pandas
+
+def lineplot(hr_year_csv):
+    # A csv file will be passed in as an argument which
+    # contains two columns -- 'HR' (the number of homerun hits)
+    # and 'yearID' (the year in which the homeruns were hit).
+    #
+    # Fill out the body of this function, lineplot, to use the
+    # passed-in csv file, hr_year.csv, and create a
+    # chart with points connected by lines, both colored 'red',
+    # showing the number of HR by year.
+    #
+    # You will want to first load the csv file into a pandas dataframe
+    # and use the pandas dataframe along with ggplot to create your visualization
+    #
+    # You can check out the data in the csv file at the link below:
+    # https://www.dropbox.com/s/awgdal71hc1u06d/hr_year.csv
+    #
+    # You can read more about ggplot at the following link:
+    # https://github.com/yhat/ggplot/
+    
+    hr_year_df = pandas.read_csv(hr_year_csv)
+    gg = ggplot(aes(x='yearID', y='HR'), data = hr_year_df) +\
+        geom_point(color='coral') + geom_line(color = 'red') +\
+        ggtitle ('HR vs Year') + xlab('Year') + ylab('Num of Homeruns')
+    return gg
